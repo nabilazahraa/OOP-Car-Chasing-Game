c2ca4d92760bc0489707f2f77614c7c73e5e7649
AmanSalat <118626998+AmanSalat@users.noreply.github.com>
2022-11-26 22:54:04 +0500

    zooo

--- PoliceCar.hpp
+++ PoliceCar.hpp
@@ -3,16 +3,19 @@
 #include <iostream>
 #include "Drawing.hpp"
 #pragma once
+using namespace std;
 class PoliceCar: public car{
     private:
     int speed;
+    int random;
     SDL_Rect srcRect, moverRect;
-    SDL_Rect sprite = {1136,27, 139,256};
-
+    SDL_Rect sprite = {1351,27, 139,256};
+    
+    int fram=0;
     public:
+    vector<Bullets*> b2; 
     PoliceCar();
     void Draw();
     void Drive(SDL_Renderer* gRenderer, SDL_Texture* assets, SDL_Keycode key);
-
-};
     
+};
