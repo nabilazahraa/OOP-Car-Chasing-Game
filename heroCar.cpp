14fa2a0d3e5a9e4537e10ab824d82b561c194e66
AmanSalat <118626998+AmanSalat@users.noreply.github.com>
2022-11-26 23:08:58 +0500

    Update heroCar.cpp

--- heroCar.cpp
+++ heroCar.cpp
@@ -6,6 +6,11 @@
 //Draw the car on the screen
 void heroCar::Draw(){
     srcRect = sprite;
+    for(int i=0;i<b.size();i++)
+    {
+        b[i]->Draw('h');
+        b[i]->ShootBullets('h');
+    }
     SDL_RenderCopy(Drawing::gRenderer, Drawing::assets, &srcRect, &moverRect);
     
 
@@ -15,8 +20,7 @@ void heroCar::Draw(){
 //sports car, bike or wagon.
 heroCar::heroCar(int x, int y, int w, int h )
 {
-    moverRect = {390,590, 100, 150};
-    speed = 5;
+    moverRect = {390,590, 50, 80};
     sprite.x = x;
     sprite.y = y;
     sprite.w = w;
@@ -28,21 +32,25 @@ heroCar::heroCar(int x, int y, int w, int h )
 void heroCar::Drive(SDL_Renderer* gRenderer, SDL_Texture* assets, SDL_Keycode key)
 {
     
-    if (key == SDLK_UP){
-        if(moverRect.y>0) //not move out of screen in y axis
-         moverRect.y-=5*speed;
+    if (key == SDLK_UP || key== SDLK_w){
+        if(moverRect.y>30) //not move out of screen in y axis
+         moverRect.y-=30;
     }
-    else if (key == SDLK_DOWN) {
-        // if(moverRect.y<600) //not move out of screen in y axis
-        moverRect.y+=5*speed;
+    else if (key == SDLK_DOWN || key==SDLK_s) {
+        if(moverRect.y<640) //not move out of screen in y axis
+        moverRect.y+=30;
     }
-    else if (key == SDLK_RIGHT) {
-        if(moverRect.x<1400) //not move out of screen in x axis
-        moverRect.x+=5*speed;
+    else if (key == SDLK_RIGHT || key==SDLK_d) {
+        if(moverRect.x<580) //not move out of screen in x axis
+        moverRect.x+=35;
     }
-    else if (key == SDLK_LEFT) {
+    else if (key == SDLK_LEFT || key==SDLK_a) {
         if(moverRect.x>150) //not move out of screen in x axis
-        moverRect.x-=5*speed;
+        moverRect.x-=35;
+    }
+    else if(key== SDLK_b){
+        Bullets *b1=new Bullets(moverRect.x,moverRect.y);
+        b.push_back(b1);
     }
     SDL_RenderCopy(Drawing::gRenderer, Drawing::assets, &srcRect, &moverRect);
 }
