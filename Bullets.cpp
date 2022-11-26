843bbddbe48b8182c800870c046d5776c92433af
AmanSalat <118626998+AmanSalat@users.noreply.github.com>
2022-11-26 22:56:03 +0500

    Update Bullets file
    This file works for both police and hero for pressing B

--- Bullets.cpp
+++ Bullets.cpp
@@ -1,1 +1,30 @@
 #include "Bullets.hpp"
+#include "Bullets.hpp"
+#include<iostream>
+#include"Drawing.hpp"
+#include<Sdl.h>
+#include"heroCar.hpp"
+
+void Bullets::Draw(char x)
+{
+    if(x=='h')
+    srcRect=sprite;
+    else
+    srcRect=sprite1;
+    SDL_RenderCopy(Drawing::gRenderer, Drawing::assets, &srcRect, &moverRect);
+}
+Bullets::Bullets()
+{
+    moverRect={440,320,20,20};
+}
+Bullets::Bullets(int x, int y)
+{
+    moverRect={x,y,20,30};
+}
+void Bullets::ShootBullets(char s)
+{
+    if(s=='h')
+    moverRect.y-=35;
+    else
+    moverRect.y+=30;
+}
