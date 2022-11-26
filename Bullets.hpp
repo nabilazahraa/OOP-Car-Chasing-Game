7da1a819a4cacb9535bd6b3651fbf4116d796f0c
AmanSalat <118626998+AmanSalat@users.noreply.github.com>
2022-11-26 22:57:36 +0500

    Update Bullets.hpp file
    class of bullets

--- Bullets.hpp
+++ Bullets.hpp
@@ -1,12 +1,14 @@
-// #include <iostream>
-// #include <SDL.h>
-
-// class Bullets
-// {
-//     SDL_Rect srcRect, moverRect;
-//     SDL_Rect sprite= {2115,23,62,170};
-
-//     public:
-//     shootBullets();
-    
-// };
+#include <iostream>
+#include <SDL.h>
+#pragma once
+class Bullets
+{
+    SDL_Rect srcRect, moverRect;
+    SDL_Rect sprite= {2330,23,62,170};
+    SDL_Rect sprite1={2128,1,107,194};
+    public:
+        void Draw(char);
+        Bullets();
+        Bullets(int x,int y);
+        void ShootBullets(char s);
+};
