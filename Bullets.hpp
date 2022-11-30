1bfb5f5329890b212b718e41b2e9cb85d8a23940
AmanSalat <118626998+AmanSalat@users.noreply.github.com>
2022-12-01 00:36:03 +0500

    Update Bullets.hpp

--- Bullets.hpp
+++ Bullets.hpp
@@ -7,8 +7,11 @@ class Bullets
     private:
     SDL_Rect srcRect= {2115,23,62,170};
     SDL_Rect moverRect;
+    SDL_Rect Boom={3539,68,312,304};
+    SDL_Rect BoomMove;
     
     public:
+        bool gone=false;
         void Draw();
         Bullets();
         Bullets(int x,int y);
