50ac15fc4fe9c4bee24828ccf9226d9aa9504f92
AmanSalat <118626998+AmanSalat@users.noreply.github.com>
2022-12-01 00:07:59 +0500

    Final file of fire.hpp

--- Fire.hpp
+++ Fire.hpp
@@ -8,9 +8,12 @@ class Fire
     private:
     SDL_Rect srcRect = {1913,1,107,194};
     SDL_Rect moverRect;
+    SDL_Rect Boom={3539,68,312,304};
+    SDL_Rect BoomMove;
 
     public:
-    void Draw();
+        void Draw();
+        bool firegone=false;
         Fire();
         Fire(int x,int y);
         void ShootFire();
